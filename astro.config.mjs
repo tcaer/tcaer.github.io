7d91c6c40c5f2da755da0f263cf5cd484bf59dfb
tcaer <tino.caer@me.com>
2026-03-15 09:48:51 +1100

    Migrate to bun + bump tailwind + astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
   site: 'https://tinocaer.com',
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
